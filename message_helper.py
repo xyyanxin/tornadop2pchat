1558cf7d79aec8d0aef9a83f43d5db7509a7dff6
严鑫 <yanxin@coucou.io>
2018-04-16 10:14:52 +0000

    add cursor in update view

--- message_helper.py
+++ message_helper.py
@@ -4,7 +4,7 @@ Program: tornadop2pserver
 Description: 
 Author: XY - mailyanxin@gmail.com
 Date: 2018-04-16 02:36:45
-Last modified: 2018-04-16 08:12:30
+Last modified: 2018-04-16 09:34:15
 Python release: 3.5.2
 """
 
@@ -40,13 +40,13 @@ class MessageBuffer(object):
         waiters = [i for i in self.waiters
                 if i['wait_target_user_id'] == from_user_id and i['wait_from_user_id'] == target_user_id]
         if len(waiters) == 1:
-            result_dict = {
+            result_list = [{
                     'message_type': ret.message_type,
                     'message_text': ret.message_text,
                     'cursor': ret.id,
-                    }
-            waiters[0]['future'].set_result(result_dict)
-            logging.info('sending to online listeners %r', self.waiters)
+                    }]
+            waiters[0]['future'].set_result(result_list)
+            logging.info('sending to online listeners')
         elif len(waiters) == 0:
             logging.info('but nobody online listen')
         else:
@@ -63,22 +63,29 @@ class MessageBuffer(object):
 
     def wait_for_message(self,wait_from_user_id,wait_target_user_id,cursor):
 
+        # waiter 列表清空
         self.refresh_waiters(wait_from_user_id,wait_target_user_id)
-
-        result_future = Future()
+        # 查新数据
+        session = DBsession()
+        new_data = session.query(MessageModel.message_type,MessageModel.message_text,MessageModel.message_url,MessageModel.id)\
+                .filter(MessageModel.from_user_id==wait_target_user_id)\
+                .filter(MessageModel.target_user_id==wait_from_user_id)
         if cursor:
-            session = DBsession()
-            new_data = session.query(MessageModel.message_type,MessageModel.message_text,MessageModel.message_url,MessageModel.id)\
-                    .filter(MessageModel.from_user_id==wait_target_user_id)\
-                    .filter(MessageModel.target_user_id==wait_from_user_id)\
-                    .filter(MessageModel.id > cursor)\
+            new_data = new_data.filter(MessageModel.id > cursor)\
                     .order_by(MessageModel.id.desc())\
                     .all()
-            ret_data = [{'message_type': i.message_type, 'message_text': i.message_text,'cursor': i.id} for i in new_data]
-            if ret_data:
-                result_future.set_result(ret_data)
-                return result_future
+        else:
+            new_data = new_data.order_by(MessageModel.id.desc())\
+                    .all()
+        session.close()
 
+        ret_data = [{'message_type': i.message_type, 'message_text': i.message_text,'cursor': i.id} for i in new_data]
+        # if future then set result 
+        result_future = Future()
+        if ret_data:
+            result_future.set_result(ret_data)
+            return result_future
+        # else add future
         foo_dict = {
                     'wait_from_user_id':wait_from_user_id,
                     'wait_target_user_id': wait_target_user_id,
